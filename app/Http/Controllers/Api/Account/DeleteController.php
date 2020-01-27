7afaba50d4610d5d676c94a95c3f634de096aa51
Nic Davies <nic@3things.dev>
2020-01-27 19:55:45 +0000

    Implemented some API controllers.

--- app/Http/Controllers/Api/Account/DeleteController.php
+++ app/Http/Controllers/Api/Account/DeleteController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers\Api\Account;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
 
 class DeleteController extends Controller
 {
-    public function __invoke(Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function __invoke(Request $request) : JsonResponse
     {
+        /** @var User $user */
+        $user = $request->user();
 
+        return response()->json([
+            'success' => true,
+        ]);
     }
 }
--- app/Http/Controllers/Api/Account/ReadController.php
+++ app/Http/Controllers/Api/Account/ReadController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers\Api\Account;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\User\UserResource;
 
 class ReadController extends Controller
 {
-    public function __invoke(Request $request)
+    /**
+     * @param Request $request
+     * @return UserResource
+     */
+    public function __invoke(Request $request) : UserResource
     {
-
+        /** @var User $user */
+        $user = $request->user();
+        return new UserResource($user);
     }
 }
