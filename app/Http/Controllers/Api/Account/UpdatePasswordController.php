59cdafa0a63474df5f162995acac6d69cb9fed23
Nic Davies <nic@3things.dev>
2020-02-09 11:09:04 +0000

    Can now update the password from account settings.

--- app/Http/Controllers/Api/Account/UpdatePasswordController.php
+++ app/Http/Controllers/Api/Account/UpdatePasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Account;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\User\UserResource;
@@ -15,19 +16,26 @@ class UpdatePasswordController extends Controller
 {
     /**
      * @param Request $request
-     * @return UserResource
+     * @return UserResource|JsonResponse
      * @throws ValidationException
      */
-    public function __invoke(Request $request) : UserResource
+    public function __invoke(Request $request)
     {
         /** @var User $user */
         $user = $request->user();
 
         $this->validate($request, [
-            'current_password'     => ['required', 'string', 'password:web'],
+            'current_password'     => ['required', 'string'],
             'new_password'         => ['required', 'string', 'min:6'],
             'confirm_new_password' => ['required', 'string', 'same:new_password'],
         ]);
+
+        if (! Hash::check($request->get('current_password'), $user->getAuthPassword())) {
+            return response()->json([
+                'error' => true,
+                'message' => 'The password is incorrect.',
+            ], 400);
+        }
 
         $user->update([
             'password' => Hash::make($request->get('new_password')),
