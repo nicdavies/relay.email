833aa05155fd50d1bebb29e4ef408f80daae7a2c
Nic Davies <nic@3things.dev>
2020-02-19 14:46:55 +0000

    Changed how we return referrals.

--- app/Http/Controllers/Api/Referral/ListController.php
+++ app/Http/Controllers/Api/Referral/ListController.php
@@ -4,26 +4,28 @@ namespace App\Http\Controllers\Api\Referral;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Referral\ReferralResource;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ListController extends Controller
 {
     /**
      * @param Request $request
-     * @return AnonymousResourceCollection
+     * @return JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request) : JsonResponse
     {
         /** @var User $user */
         $user = $request->user();
 
-        $referrals = $user
-            ->referrals()
-            ->paginate(15)
-        ;
+        $referrals = $user->referrals();
 
-        return ReferralResource::collection($referrals);
+        return response()->json([
+            'data' => [
+                'total' => $referrals->count(),
+                'referrals' => ReferralResource::collection($referrals->paginate(20)),
+            ],
+        ]);
     }
 }
