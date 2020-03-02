0192b496f10f9f97cd0a0b73de843d4ea90924c4
Nic Davies <nic@3things.dev>
2020-03-02 18:21:18 +0000

    Removed dead code from InboundController.php

--- app/Http/Controllers/Api/Webhook/InboundController.php
+++ app/Http/Controllers/Api/Webhook/InboundController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api\Webhook;
 
-use Mailgun\Mailgun;
 use Illuminate\Http\Request;
 use App\Jobs\InboundEmailJob;
 use Illuminate\Http\JsonResponse;
@@ -11,17 +10,6 @@ use Illuminate\Validation\ValidationException;
 
 class InboundController extends Controller
 {
-//    private Mailgun $mailgun;
-//
-//    /**
-//     * InboundController constructor.
-//     * @param Mailgun $mailgun
-//     */
-//    public function __construct(Mailgun $mailgun)
-//    {
-//        $this->mailgun = $mailgun;
-//    }
-
     /**
      * @param Request $request
      * @return JsonResponse
@@ -29,19 +17,6 @@ class InboundController extends Controller
      */
     public function __invoke(Request $request) : JsonResponse
     {
-//        $this->validate($request, [
-//            '' => [],
-//        ]);
-
-        // todo - validate the incoming post data
-        // todo - make sure it came from mailgun
-
-//        $this->mailgun->webhooks()->verifyWebhookSignature(
-//            $request->get('timestamp'),
-//            $request->get('token'),
-//            $request->get('signature')
-//        );
-
         // Fire off a job to handle this payload so we don't keep mailgun waiting
         InboundEmailJob::dispatch($request);
 
